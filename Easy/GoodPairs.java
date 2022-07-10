f0285ab89e46dc132333a7b311c8fc090d411bac
Aman Mishra <mishraaman113114@gmail.com>
2022-07-10 23:32:56 +0530

    Number of good pairs

---
+++ Easy/GoodPairs.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class GoodPairs {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(numIdenticalPairs(arr));
+        sc.close();
+    }
+
+    private static int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for(int i = 0; i< nums.length; i++){
+            for(int j = i; j < nums.length; j++){
+                if(nums[i] == nums[j] && i < j){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+}
