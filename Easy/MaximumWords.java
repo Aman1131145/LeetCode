9d2fd016dd0e92df5a379e6df35756d38f41bc83
Aman Mishra <mishraaman113114@gmail.com>
2022-07-10 23:28:59 +0530

    Maximum words

---
+++ Easy/MaximumWords.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * MaximumWords
+ */
+public class MaximumWords {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String sentences[] = new String[n];
+        for(int i = 0; i < n; i++){
+            sentences[i] = sc.next();
+        }
+        System.out.println(mostWordsFound(sentences));
+        sc.close();
+    }
+    public static int mostWordsFound(String[] sentences) {
+        int ans = 0;
+        for(String s : sentences){
+            int num = 1;
+            for(int i = 0; i < s.length(); i++){
+                if(s.charAt(i) == ' '){
+                    num++;
+                }
+            }
+            if(num > ans){
+                ans = num;
+            }
+        }
+        return ans;
+    }
+}
