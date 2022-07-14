e7e02f3baabad2349377753966d115cf29957550
Aman Mishra <mishraaman113114@gmail.com>
2022-07-14 15:06:48 +0530

    Subtract the product and sum of digits of an integer

---
+++ Easy/HighestAcceptance/SubtractProductAndSum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int subtractProductAndSum(int n) {
+        
+        int sum = 0;
+        int product = 1;
+        while(n > 0){
+            int temp = n%10;
+            n = n/10;
+            sum += temp;
+            product *= temp;
+        } 
+        return product - sum;
+    }
+}
