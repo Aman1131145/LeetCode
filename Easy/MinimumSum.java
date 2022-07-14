be48048cc72dca13b23528e120e8573eaaf605b3
Aman Mishra <mishraaman113114@gmail.com>
2022-07-14 14:33:57 +0530

    Minimum sum

--- Easy/MinimumSum.java
+++ Easy/MinimumSum.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class MinimumSum {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
