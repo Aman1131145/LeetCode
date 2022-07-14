26983f42e164ca12bee64f15da89079f320ea800
Aman Mishra <mishraaman113114@gmail.com>
2022-07-14 14:34:17 +0530

    Maximum words

--- Easy/MaximumWords.java
+++ Easy/MaximumWords.java
@@ -5,15 +5,19 @@ import java.util.Scanner;
  */
 public class MaximumWords {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String sentences[] = new String[n];
-        for(int i = 0; i < n; i++){
-            sentences[i] = sc.next();
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            String sentences[] = new String[n];
+            for(int i = 0; i < n; i++){
+                sentences[i] = sc.next();
+            }
+            System.out.println(mostWordsFound(sentences));
+            sc.close();
         }
-        System.out.println(mostWordsFound(sentences));
-        sc.close();
     }
     public static int mostWordsFound(String[] sentences) {
         int ans = 0;
